d45bace84c21170abe99cb7cbf2c987589d54a2c
UltraViolet33 <93035200+UltraViolet33@users.noreply.github.com>
2024-09-01 07:06:55 +0200

    Update command.sh

--- AD9_Linux_web/command.sh
+++ AD9_Linux_web/command.sh
@@ -10,10 +10,10 @@ ls | grep log | wc -l
 
 
 # Affichez chaque ligne dans tous les fichiers .txt d'un répertoire qui contient les code "500"
-grep -r "500" *
+grep "500" *.txt
 
 # écrit ces lignes dans le fichier all_five_hundred.log
 grep -r "500" * > all_five_hundred.log
 
 # déplace tous les fichiers jpeg du dossier courant dans le dossier images du dossier courant
-mv ./*.jpeg images/
+mv **/*.jpeg images/
