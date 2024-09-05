a1785215d813a0054f4ec4e090dc614834c96529
DanielaPimenko <d.silva@pimenko.com>
2024-09-05 16:00:42 +0200

    structure dossier

--- script.sh
+++ script.sh
@@ -1,7 +1,51 @@
+
+dir_proj="project"
+dir_rel="release"
+dir_sha="shared"
 date_folder=$(date +"%Y-%m-%d-%T")
+dir_lib="lib"
+dir_company="thecompanylegacynotversionnedlibrary"
 
-dir_repo="release/"
+create_project(){
+  mkdir "$dir_proj"
+  cd "$dir_proj"
+  mkdir "$dir_rel"
+  mkdir -p "$dir_sha/$dir_lib"
+  mkdir -p "$dir_sha/$dir_lib/$dir_company"
+}
 
-cd $dir_repo
+create_date_dir(){
+  cd "$dir_proj/$dir_rel"
+  mkdir "$date_folder"
+}
 
-mkdir "$date_folder"
+deletus(){
+  rm -r `ls -t | tail -n +6`
+}
+
+OPTSTRING=":abc"
+
+while getopts ${OPTSTRING} opt; do
+  case ${opt} in
+    a)
+       echo "Directories deleted"
+       deletus
+      ;;
+    b)
+      echo "Option create project was selected"
+      create_project
+      ;;
+    c)
+      echo "Create date directory"
+      create_date_dir
+      ;;
+    ?)
+      echo "Invalid option: -${OPTARG}."
+      exit 1
+      ;;
+  esac
+done
+
+
+
+
