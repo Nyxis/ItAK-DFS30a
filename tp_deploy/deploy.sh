8a62cf447732910b147acba438abc07910133629
UltraViolet33 <93035200+UltraViolet33@users.noreply.github.com>
2024-09-06 16:54:29 +0200

    clone repo from script

--- tp_deploy/deploy.sh
+++ tp_deploy/deploy.sh
@@ -1,80 +1,146 @@
-printf -v date '%(%Y%m%d%H%M%S)T' -1
+#!/bin/bash
 
-if [ "$1" = "deploy" ]; then 
+source .env
 
-  # create release folder
-  mkdir project/release/$date
+deploy () {
+    printf -v date '%(%Y%m%d%H%M%S)T' -1
+    echo "create project/release/$date"
 
+    mkdir project/release/$date
 
-  # delete old release function
-  delete_releases () {
-      releases=($(ls -t  ./project/release/ ))
-      nb_releases=${#releases[@]}
-      b=$nb_to_delete
+    clone_project
 
-      # if nb releases > 5
-      if [ "$nb_releases" -gt "$b" ] 
-      then
-          releases_to_delete=("${releases[@]:b}")
-          for i in "${releases_to_delete[@]}"
-          do
-              rm -rf ./project/release/"$i"
-          done
-      fi
-  }
+    # copy_shared_to_release
 
-  # get arg releases nb to delete
-  OPTSTRING=":x:"
+    delete_releases
+}
 
-  nb_to_delete=5
-  while getopts ${OPTSTRING} opt; do
-    case ${opt} in
-      x)
-        nb_to_delete=${OPTARG}
-        echo "Option -x was triggered, Argument: ${OPTARG}"
-        ;;
-      y)
-        echo "Option -y was triggered, Argument: ${OPTARG}"
-        ;;
-      :)
-        echo "Option -${OPTARG} requires an argument."
-        exit 1
-        ;;
-      ?)
-        echo "Invalid option: -${OPTARG}."
-        exit 1
-        ;;
-    esac
-  done
+copy_shared_to_release () {
+    shared_file=($(cd ./project/shared/ && ls -pa | grep -v / ))
 
+    for i in "${shared_file[@]}"
+        do
+            echo ln -s ./project/shared/$i ./project/release/$date/$i 
+            ln -s ./project/shared/$i ./project/release/$date/$i
+    done
+}
 
+delete_releases () {
+    releases=($(ls -t  ./project/release/ ))
+    nb_releases=${#releases[@]}
+    nb_to_delete=10
 
-  # link shared/files to release
-  shared_file=($(cd ./project/shared/ && ls -pa | grep -v / ))
+    if [ "$nb_releases" -gt "$nb_to_delete" ] 
+    then
+        releases_to_delete=("${releases[@]:nb_to_delete}")
+        for i in "${releases_to_delete[@]}"
+        do
+            rm -rf ./project/release/"$i"
+        done
+    fi
+}
 
-  for i in "${shared_file[@]}"
+clone_project () {
+#   rm ./project/current
 
-    do
-      echo $i
-      echo ./project/shared/$i
-      echo ./project/release/$date/$i
-        ln -s ./project/shared/$i ./project/release/$date/$i
-  done
+  cd ./../AD9_Linux_web/
+  git clone -b main $REMOTE clone_me 
+  cd ../tp_deploy/
+  echo $(pwd)
+  cp -r ./../AD9_Linux_web/clone_me/* ./project/release/$date/
 
-  # create current link from current release
-  rm ./project/current
-  ln -s ./project/release/$date ./project/current
+    FILE=$(pwd)/project/release/$date/AD9_Linux_web/clone_me/
+
+    if [ -f $FILE/Makefile ]; then
+        cd $FILE
+        make build
+    fi
+
+    cd ../../../../../
+    ln -s $(pwd)/project/release/$date $(pwd)/project/current
+}
 
 
-  delete_releases "$nb_to_delete"
-
-# update current link with the previous release
-elif [ "$1" = "rollback" ]; then
-
+rollback () {
     last_two_release=($(ls project/release/ -t | head -2 ))
     last_two_release2=("${last_two_release[@]:b}")
     previous_release=${last_two_release2[1]}
+    project_files=($(ls -f project/ ))
 
-    rm ./project/current
+    for i in "${project_files[@]}"
+        do
+            if [ "$i" = "current" ]; then
+                rm -rf ./project/"$i"
+            fi
+    done
+
+    echo "update current release link"
     ln -s ./project/release/$previous_release ./project/current
+}
+
+
+clean () {
+    rm -rf ../AD9_Linux_web/clone_me
+}
+
+if [ "$1" = "deploy" ]; then
+    echo "deploy mode"
+    deploy
+elif [ "$1" = "rollback" ]; then
+    echo "rollback mode"
+    rollback
+
+elif [ "$1" = "clean" ]; then
+    clean
 fi
+
+
+
+
+    
+
+
+
+
+# # create release folder
+
+# # delete old release function
+# delete_releases () {
+#     releases=($(ls -t  ./project/release/ ))
+#     nb_releases=${#releases[@]}
+#     b=$nb_to_delete
+
+#     # if nb releases > 5
+#     if [ "$nb_releases" -gt "$b" ] 
+#     then
+#         releases_to_delete=("${releases[@]:b}")
+#         for i in "${releases_to_delete[@]}"
+#         do
+#             rm -rf ./project/release/"$i"
+#         done
+#     fi
+# }
+
+# # get arg releases nb to delete
+# OPTSTRING=":x:"
+
+# nb_to_delete=5
+# while getopts ${OPTSTRING} opt; do
+# case ${opt} in
+#     x)
+#     nb_to_delete=${OPTARG}
+#     echo "Option -x was triggered, Argument: ${OPTARG}"
+#     ;;
+#     :)
+#     echo "Option -${OPTARG} requires an argument."
+#     exit 1
+#     ;;
+#     ?)
+#     echo "Invalid option: -${OPTARG}."
+#     exit 1
+#     ;;
+# esac
+# done
+
+# delete_releases "$nb_to_delete"
+
